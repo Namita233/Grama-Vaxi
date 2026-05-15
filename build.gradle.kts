cc84d7392f338e194f78da3a7aa7052575e99f13
Namita Govind Naik <Namiengcs25@gmail.com>
2026-05-15 13:01:37 +0530

    Initial Grama-Vaxi project upload

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file
+
+plugins {
+
+    alias(libs.plugins.android.application) apply false
+
+    kotlin("android") version "2.0.21" apply false
+
+    kotlin("kapt") version "2.0.21" apply false
+}
